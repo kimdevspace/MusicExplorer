7c7167ff84c8c1a4e345e85e147f9214da0ad04b
김의중 <ejk5148@gmail.com>
2025-04-03 15:51:41 +0900

    init: 스프링부트 시작

---
+++ musicexplorerapi/src/test/java/com/kimdevspace/music_explorer_api/MusicExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimdevspace.music_explorer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
