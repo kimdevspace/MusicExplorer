ed9dbd6b1373574099e2aa3e0a6a409bfbefd3b2
김의중 <ejk5148@gmail.com>
2025-04-03 16:09:49 +0900

    feat: 스프링부트 시작

---
+++ musicexplorer/src/main/java/com/kimdevspace/musicexplorer/MusicexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.kimdevspace.musicexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicexplorerApplication.class, args);
+	}
+
+}
---
+++ musicexplorer/src/test/java/com/kimdevspace/musicexplorer/MusicexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimdevspace.musicexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
